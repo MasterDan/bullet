199401d45ae5e39abcb3f25b89719714ad27c248
MassterDanny <MihaylovDS@gmail.com>
2021-06-07 21:01:16 +0300

    still testing

--- src/Core/proxy/arrayProxy.test.ts
+++ src/Core/proxy/arrayProxy.test.ts
@@ -5,13 +5,22 @@ describe('array tests', () => {
   test('Change Value Behavior', () => {
     const array = makeArrayReactive(['Apple', 'Orange', 'Pineapple']);
     let detector = '';
+    let changeDetector = [];
     const sub: Subscribtion<string> = jest.fn((val, old) => {
       detector = `${old}-${val}`;
     });
+    const changeSub: Subscribtion<string[]> = jest.fn((val) => {
+      expect(val).not.toBeNull();
+      expect(val).not.toBeUndefined();
+      changeDetector = val;
+    });
     array.subscribeElement(0, sub);
+    array.subscribe(changeSub);
     array[0] = 'Pear';
+    expect(changeSub).toBeCalled();
     expect(sub).toBeCalled();
     expect(detector).toBe('Apple-Pear');
+    expect(changeDetector).toEqual(['Pear', 'Orange', 'Pineapple']);
     return undefined;
   });
 });
