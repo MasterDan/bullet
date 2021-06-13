40a120f36f87b1301152809701e036dc05f2f6ef
MasterDan <MihaylovDS@gmail.com>
2021-06-13 22:42:37 +0300

    Array check

--- src/Core/tools/checkers.test.ts
+++ src/Core/tools/checkers.test.ts
@@ -1,4 +1,4 @@
-import { isFunction, isObject } from './checkers';
+import { isArray, isFunction, isObject } from './checkers';
 
 class fooClass {
   a: 3;
@@ -9,22 +9,36 @@ describe('Checkers', () => {
     a: 1,
     b: true
   };
+  const fooArr = [2, 1, 6, 5];
   const fooFun = () => 42;
   const fooClassObj = new fooClass();
   const fooStr = 'hello world';
   const fooDate = new Date();
   test('isFunction', () => {
-    expect(isFunction(fooObj)).toBe(false);
     expect(isFunction(fooFun)).toBe(true);
+
+    expect(isFunction(fooObj)).toBe(false);
+    expect(isFunction(fooObj)).toBe(false);
     expect(isFunction(fooClassObj)).toBe(false);
     expect(isFunction(fooStr)).toBe(false);
     expect(isFunction(fooDate)).toBe(false);
   });
   test('isObject', () => {
     expect(isObject(fooObj)).toBe(true);
-    expect(isObject(fooFun)).toBe(false);
     expect(isObject(fooClassObj)).toBe(true);
+
+    expect(isObject(fooArr)).toBe(false);
+    expect(isObject(fooFun)).toBe(false);
     expect(isObject(fooStr)).toBe(false);
     expect(isObject(fooDate)).toBe(false);
+  });
+  test('isArray', () => {
+    expect(isArray(fooArr)).toBe(true);
+
+    expect(isArray(fooObj)).toBe(false);
+    expect(isArray(fooFun)).toBe(false);
+    expect(isArray(fooClassObj)).toBe(false);
+    expect(isArray(fooStr)).toBe(false);
+    expect(isArray(fooDate)).toBe(false);
   });
 });
--- src/Core/tools/checkers.ts
+++ src/Core/tools/checkers.ts
@@ -1,6 +1,12 @@
 export function isObject(val: unknown): boolean {
-    return typeof val === 'object' && !(val instanceof Date);
+  return (
+    typeof val === 'object' && !(val instanceof Date) && !Array.isArray(val)
+  );
 }
 export function isFunction(val: unknown): boolean {
-    return typeof val === 'function';
+  return typeof val === 'function';
+}
+
+export function isArray(val: unknown): boolean {
+  return Array.isArray(val);
 }
