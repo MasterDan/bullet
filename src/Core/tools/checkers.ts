85da8b8d41f880fd204c775615cd7c82c590dd26
MasterDan <MihaylovDS@gmail.com>
2021-06-15 14:14:25 +0300

    Type Assertion in Checkers

--- src/Core/tools/checkers.ts
+++ src/Core/tools/checkers.ts
@@ -1,12 +1,14 @@
-export function isObject(val: unknown): boolean {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isObject(val: unknown): val is object {
   return (
     typeof val === 'object' && !(val instanceof Date) && !Array.isArray(val)
   );
 }
-export function isFunction(val: unknown): boolean {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isFunction(val: unknown): val is Function {
   return typeof val === 'function';
 }
 
-export function isArray(val: unknown): boolean {
+export function isArray(val: unknown): val is Array<unknown> {
   return Array.isArray(val);
 }
