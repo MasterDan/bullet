d91753a1c06beb30c339b130c73e2a7db30f4810
MasterDan <MihaylovDS@gmail.com>
2021-07-19 21:01:14 +0300

    draft

--- src/Core/component/component.ts
+++ src/Core/component/component.ts
@@ -4,3 +4,17 @@ class BulletInterface<
 > {
   constructor(public props: TProps, public emits: TEmits) {}
 }
+
+class BulletComponent<
+  TProps extends Record<string, unknown>,
+  TEmits extends Record<string, unknown>,
+  TDef extends BulletInterface<TProps, TEmits>
+> {
+  constructor(public __interface: TDef) {}
+}
+
+function Bullet(numOfWheels: number) {
+  return function (constructor: Function) {
+    constructor.prototype.wheels = numOfWheels;
+  };
+}
