f7a04f485d2f47a974f019c730f798f1e12de89f
MasterDan <MihaylovDS@gmail.com>
2021-07-23 14:12:00 +0300

    Component class

--- src/Core/component/component.ts
+++ src/Core/component/component.ts
@@ -1,3 +1,5 @@
+import { ReactiveObjectProxy } from '../proxy/objectProxy';
+import { BulletRootNode } from '../template/bulletRootNode';
 class BulletInterface<
   TProps extends Record<string, unknown>,
   TEmits extends Record<string, unknown>
@@ -8,13 +10,13 @@ class BulletInterface<
 class BulletComponent<
   TProps extends Record<string, unknown>,
   TEmits extends Record<string, unknown>,
-  TDef extends BulletInterface<TProps, TEmits>
+  TDef extends BulletInterface<TProps, TEmits>,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  TObj extends object
 > {
-  constructor(public __interface: TDef) {}
-}
-
-function Bullet(numOfWheels: number) {
-  return function (constructor: Function) {
-    constructor.prototype.wheels = numOfWheels;
-  };
+  constructor(
+    public __interface: TDef,
+    public __data: ReactiveObjectProxy<TObj>,
+    public __template: BulletRootNode
+  ) {}
 }
