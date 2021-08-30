c5096c746203dcff1c720c692e0f2c7bc7e0d21f
MasterDan <MihaylovDS@gmail.com>
2021-08-30 20:31:46 +0300

    directive

--- src/Core/bullet/context/directives/bulletDirective.ts
+++ src/Core/bullet/context/directives/bulletDirective.ts
@@ -1,3 +1,5 @@
-export class BulletDirective {
-  expression: string;
+export abstract class BulletDirective {
+  abstract expression: string;
+  abstract bind<T>(el: HTMLElement, value: T): void;
+  abstract destroy<T>(el: HTMLElement, value: T): void;
 }
