50e60ba3ec5b96a3377daf04b0c11521c89698cf
MasterDan <MihaylovDS@gmail.com>
2021-09-10 20:23:13 +0300

    Created default binder

--- src/Core/component/componentHooks.ts
+++ src/Core/component/componentHooks.ts
@@ -1,4 +1,6 @@
 import { Emitter } from '../reactive/emitter';
+import { Subscribtion } from '../reactive/subscribtion';
+import { Component } from './component';
 
 export class ComponentHooks {
   created: Emitter<void>;
@@ -6,4 +8,23 @@ export class ComponentHooks {
   update: Emitter<void>;
   mounted: Emitter<void>;
   dispose: Emitter<void>;
+}
+
+export class ComponentHookBinder {
+  onMount: Subscribtion<void>;
+  onUpdate: Subscribtion<void>;
+  constructor(arg: {
+    onMount: Subscribtion<void>;
+    onUpdate: Subscribtion<void>;
+  }) {
+    this.onMount = arg.onMount;
+    this.onUpdate = arg.onUpdate;
+  }
+  bindTo<
+    TProps extends Record<string, unknown>,
+    TEmits extends Record<string, Emitter<unknown>>
+  >(component: Component<TProps, TEmits>): void {
+    component.__hooks.mount.subscribe(this.onMount);
+    component.__hooks.update.subscribe(this.onUpdate);
+  }
 }
